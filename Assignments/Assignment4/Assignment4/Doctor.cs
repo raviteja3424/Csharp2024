1dd7886653eafc2c0f7527fa9da650889bf2556f
Raviteja Booraga <ravitejabo@infinite.com>
2024-07-01 16:16:39 +0530

    Assignment4

---
+++ Assignments/Assignment4/Assignment4/Doctor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    class Doctor
+    {
+        private int RegnNo { get; set; }
+        private string Name { get; set; }
+        private decimal FeesCharged { get; set; }
+
+        public Doctor(int regnNo, string name, decimal feesCharged)
+        {
+            RegnNo = regnNo;
+            Name = name;
+            FeesCharged = feesCharged;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Registration Number: " + RegnNo);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Fees Charged: " + FeesCharged);
+            Console.ReadLine();
+        }
+    }
+
+    public class doctor
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Registration Number:");
+            int regnNo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Doctor's Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Fees Charged:");
+            decimal feesCharged = decimal.Parse(Console.ReadLine());
+
+            Doctor doctor = new Doctor(regnNo, name, feesCharged);
+            doctor.DisplayDetails();
+        }
+    }
+}
---
+++ Assignments/Assignment4/Assignment4/Scholarship.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    class Scholarship
+    {
+        public decimal Merit(int marks, decimal fees)
+        {
+            decimal scholarshipAmount = 0;
+
+            if (marks >= 70 && marks <= 80)
+            {
+                scholarshipAmount = 0.2m * fees;
+            }
+            else if (marks > 80 && marks <= 90)
+            {
+                scholarshipAmount = 0.3m * fees;
+            }
+            else if (marks > 90)
+            {
+                scholarshipAmount = 0.5m * fees;
+            }
+
+            return scholarshipAmount;
+        }
+    }
+    public class program
+    {
+
+        static void Main()
+        {
+            Scholarship scholarship = new Scholarship();
+
+            Console.WriteLine("Enter marks:");
+            int marks = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter fees:");
+            decimal fees = decimal.Parse(Console.ReadLine());
+
+            decimal scholarshipAmount = scholarship.Merit(marks, fees);
+            Console.WriteLine("Scholarship amount for marks " + marks + " and fees " + fees + ": " + scholarshipAmount);
+            Console.Read();
+        }
+    }
+
+}
