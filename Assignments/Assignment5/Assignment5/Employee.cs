05bdb0262e02e5ec4ea55dd2a0b1e4042f0b10d2
Raviteja Booraga <ravitejabo@infinite.com>
2024-07-02 11:28:09 +0530

    Assignment5

---
+++ Assignments/Assignment5/Assignment5/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    public class Employee
+    {
+        public int Empid { get; set; }
+        public string Empname { get; set; }
+        public float Salary { get; set; }
+
+        public Employee(int empid, string empname, float salary)
+        {
+            Empid = empid;
+            Empname = empname;
+            Salary = salary;
+        }
+    }
+
+    public class ParttimeEmployee : Employee
+    {
+        public float Wages { get; set; }
+
+        public ParttimeEmployee(int empid, string empname, float salary, float wages)
+            : base(empid, empname, salary)
+        {
+            Wages = wages;
+        }
+    }
+
+    class employee
+    {
+        static void Main(string[] args)
+        {
+            ParttimeEmployee partTimeEmp = new ParttimeEmployee(1, "Ravi", 55000, 100);
+            Console.WriteLine("Emp ID: " + partTimeEmp.Empid + ", Name: " + partTimeEmp.Empname + ", Salary: " + partTimeEmp.Salary + ", Wages: " + partTimeEmp.Wages);
+            Console.Read();
+        }
+    }
+}
