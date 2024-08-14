55996f2cd332599666410a1f465d21c84f8431aa
raviteja3424 <raviteja.booraga@infinite.com>
2024-08-14 12:39:09 +0530

    CC9

---
+++ Assessments/MVCAssessments/CC9/Question1/MVC(CC9)/Controllers/CodeController.cs
@@ -0,0 +1,34 @@
+﻿using MVC_CC9_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CC9_.Controllers;
+
+namespace CodeController.Module
+{
+    public class CodeController : Controller
+    {
+        private NorthWindEntities db = new NorthWindEntities();
+        public ActionResult CustomersInGermany()
+        {
+            var customers = db.Customers
+                              .Where(c => c.Country == "Germany")
+                              .ToList();
+
+            return View(customers);
+        }
+
+        
+        public ActionResult CustomerDetailsByOrder(int orderId = 10248)
+        {
+            var order = db.Orders
+                          .Where(o => o.OrderID == orderId)
+                          .Select(o => o.Customer)
+                          .FirstOrDefault();
+
+            return View(order);
+        }
+    }
+}
